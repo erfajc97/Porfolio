0f6ac5675fe89b3e6a2d392ff850d8e572654148
Erick <erfacrypto@gmail.com>
2023-02-08 16:22:38 -0300

    img appear

--- src/assets/components/Portafolio.jsx
+++ src/assets/components/Portafolio.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import img1 from "../Img/ImgPortafolio/img1.png";
 import img2 from "../Img/ImgPortafolio/img2.png";
 import img3 from "../Img/ImgPortafolio/img3.png";
@@ -9,8 +9,27 @@ import img7 from "../Img/ImgPortafolio/img7.png";
 import img8 from "../Img/ImgPortafolio/img8.png";
 
 const Portafolio = () => {
+  const [on, setOn] = useState(true);
+  const array = [img1, img2, img3, img4, img5, img6, img7, img8];
+  const [numberArray, setNumberArray] = useState(0);
+  const appear = (num) => {
+    setOn(!on);
+    setNumberArray(num);
+  };
+
   return (
     <div className="container_principal_porfolio">
+      <div
+        style={{ display: on ? "flex" : "none" }}
+        className="container_info_img_por">
+        <div className="container_info_descrip_button">
+          <img className="img_porf_appear" src={array[numberArray]} />
+          <button onClick={appear} className="btn_x_img">
+            <i className="bx bx-x bx-lg"></i>
+          </button>
+        </div>
+      </div>
+
       <div className="container_title_porfolio">
         <h2 id="porfolio" className="title_portfolio">
           Portfolio
@@ -32,10 +51,9 @@ const Portafolio = () => {
 
       <div className="container_img_portfolio">
         <div className="container_info_img_porfolio">
-         
           <img className="img_porf" id="img6" src={img6} alt="" />
           <div className="container_buttons_porfolio">
-            <p className="btn-links_porfolio">
+            <p onClick={() => appear(5)} className="btn-links_porfolio">
               <i className="icon-pf bx bx-plus bx-lg"></i>
             </p>
             <hr />
@@ -52,7 +70,7 @@ const Portafolio = () => {
         <div className="container_info_img_porfolio">
           <img className="img_porf" id="img7" src={img7} alt="" />
           <div className="container_buttons_porfolio">
-            <p className="btn-links_porfolio">
+            <p onClick={() => appear(6)} className="btn-links_porfolio">
               <i className="icon-pf bx bx-plus bx-lg"></i>
             </p>
             <hr />
@@ -69,7 +87,7 @@ const Portafolio = () => {
         <div className="container_info_img_porfolio">
           <img className="img_porf" id="img8" src={img8} alt="" />
           <div className="container_buttons_porfolio">
-            <p className="btn-links_porfolio">
+            <p onClick={() => appear(7)} className="btn-links_porfolio">
               <i className="icon-pf bx bx-plus bx-lg"></i>
             </p>
             <hr />
@@ -86,7 +104,7 @@ const Portafolio = () => {
         <div className="container_info_img_porfolio">
           <img className="img_porf" id="img5" src={img5} alt="" />
           <div className="container_buttons_porfolio">
-            <p className="btn-links_porfolio">
+            <p onClick={() => appear(4)} className="btn-links_porfolio">
               <i className="icon-pf bx bx-plus bx-lg"></i>
             </p>
             <hr />
@@ -103,7 +121,7 @@ const Portafolio = () => {
         <div className="container_info_img_porfolio">
           <img className="img_porf" id="img1" src={img1} alt="" />
           <div className="container_buttons_porfolio">
-            <p className="btn-links_porfolio">
+            <p onClick={() => appear(0)} className="btn-links_porfolio">
               <i className="icon-pf bx bx-plus bx-lg"></i>
             </p>
             <hr />
@@ -120,7 +138,7 @@ const Portafolio = () => {
         <div className="container_info_img_porfolio">
           <img className="img_porf" id="img2" src={img2} alt="" />
           <div className="container_buttons_porfolio">
-            <p className="btn-links_porfolio">
+            <p onClick={() => appear(1)} className="btn-links_porfolio">
               <i className="icon-pf bx bx-plus bx-lg"></i>
             </p>
             <hr />
@@ -137,7 +155,7 @@ const Portafolio = () => {
         <div className="container_info_img_porfolio">
           <img className="img_porf" id="img3" src={img3} alt="" />
           <div className="container_buttons_porfolio">
-            <p className="btn-links_porfolio">
+            <p onClick={() => appear(2)} className="btn-links_porfolio">
               <i className="icon-pf bx bx-plus bx-lg"></i>
             </p>
             <hr />
@@ -154,7 +172,7 @@ const Portafolio = () => {
         <div className="container_info_img_porfolio">
           <img className="img_porf" id="img4" src={img4} alt="" />
           <div className="container_buttons_porfolio">
-            <p className="btn-links_porfolio">
+            <p onClick={() => appear(3)} className="btn-links_porfolio">
               <i className="icon-pf bx bx-plus bx-lg"></i>
             </p>
             <hr />
